e4c6172723b2b2977b8dd14d0f544a53c3d3e014
Code Sandbox <hello@codesandbox.io>
2023-03-12 17:10:12 +0000

    Deployment from Codesandbox.

---
+++ script.js
@@ -0,0 +1,37 @@
+// lenis
+
+const lenis = new Lenis({
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
+  direction: "vertical", // vertical, horizontal
+  gestureDirection: "vertical", // vertical, horizontal, both
+  smooth: true,
+  mouseMultiplier: 1,
+  smoothTouch: false,
+  touchMultiplier: 2,
+  infinite: false
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+// generic
+gsap.registerPlugin(ScrollTrigger);
+
+// apply parallax effect to any element with a data-speed attribute
+gsap.to("[data-speed]", {
+  y: (i, el) =>
+    (1 - parseFloat(el.getAttribute("data-speed"))) *
+    ScrollTrigger.maxScroll(window),
+  ease: "none",
+  scrollTrigger: {
+    start: 0,
+    end: "max",
+    invalidateOnRefresh: true,
+    scrub: 0
+  }
+});
